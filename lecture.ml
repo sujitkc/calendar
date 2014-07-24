91c012cb70ef93389340320bebe014924828a820
sujitkc <sujitkc@gmail.com>
2014-07-24 09:23:59 +0530

    added comments to lecture.ml

--- lecture.ml
+++ lecture.ml
@@ -1,6 +1,24 @@
+(*
+  This file contains an example of application of the calendar library. The objective of this program
+  is to print the list of dates (along with their weekdays) on which the lectures of a particular
+  course will be held. Inputs to this program are: the duration of the semester in terms of its 
+  starting and ending dates, and the week days on which the lectures are held for the course.
+  See test cases at the bottom of the file for examples.
+*)
+
+(*
+  Given a pair (d, wd) where d is a date and wd is a weeday, return its string representation.
+  Example:
+    ((2, January, 2014), Thursday) --> "January 2, 2014, Thursday"
+*)
 let string_of_date_weekday (d, wd) =
   (Calendar.string_of_date d) ^ ", " ^ (Calendar.string_of_weekday wd)
 
+(*
+  Given a list lst of dates, return a list where each element is a date in the lst, coupled with its weekday.
+  Example:
+     [(2, January, 2014); (3, January, 2014)]--> [((2, January, 2014), Thursday); ((3, January, 2014), Friday)]
+*)
 let weekdayMap lst =
   List.map (fun d -> (d, (Calendar.getWeekDay d))) lst
 

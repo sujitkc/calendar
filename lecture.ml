40cde6675cbaef54a19545cb7907b93ef27900de
sujitkc <sujitkc@gmail.com>
2015-01-16 14:46:09 +0530

    - Added function: getLectureDatesWithHolidayList to lecture.ml. This function is similar to getLectureDates except that it also takes into account the additional list of holidays. The resulting list doesn't contain any of the members of the provided holiday list. - Also defined the type holidays in the process.

--- lecture.ml
+++ lecture.ml
@@ -6,6 +6,14 @@
   See test cases at the bottom of the file for examples.
 *)
 
+(* DATA DEFINITIONS *)
+(*
+The holidays type may be either a single holiday or a vacation having a range of contiguous dates.
+*)
+type holidays =
+    Holiday  of (int * Calendar.month * int)
+  | Vacation of (int * Calendar.month * int) * (int * Calendar.month * int) 
+
 (*
   Given a pair (d, wd) where d is a date and wd is a weeday, return its string representation.
   Example:
@@ -13,6 +21,18 @@
 *)
 let string_of_date_weekday (d, wd) =
   (Calendar.string_of_date d) ^ ", " ^ (Calendar.string_of_weekday wd)
+
+
+let flattenHolidayList hlist =
+  let flatten = function
+    Holiday(h)       -> [h]
+  | Vacation(d1, d2) -> (Calendar.dateRange d1 d2)
+  in
+  let rec iter acc = function
+     []       -> acc
+   | hol :: t -> (iter (acc @ (flatten hol)) t)
+  in
+  iter [] hlist
 
 (*
   Given a list lst of dates, return a list where each element is a date in the lst, coupled with its weekday.
@@ -23,6 +43,12 @@ let weekdayMap lst =
   List.map (fun d -> (d, (Calendar.getWeekDay d))) lst
 
 let (|>) f g x = g(f x)
+
+let printDateList l =
+    List.iter (fun s -> (print_string (Calendar.string_of_date s ^ "\n"))) l
+
+let printDateWeekdayList l =
+    List.iter (fun s -> (print_string (string_of_date_weekday s ^ "\n"))) l
 
 (*
   Given two dates d1 and d2, and a list lec of weekdays on which the lectures are 
@@ -45,21 +71,74 @@ let getLectureDates d1 d2 lec =
     in
   (dr |> weekdayMap |> f) (d1, d2)
 
-(* TEST CASES *)
-let printDateList l =
-    List.iter (fun s -> (print_string (string_of_date_weekday s ^ "\n"))) l
+(*
+  Given:
+    1) two dates d1 and d2
+    2) a list lec of weekdays on which the lectures are to be held
+    3) A holiday list,
+  Return: the list of pairs (date, weekday) for each lecture day.
+  Example:
+    getLectureDatesWithHolidayList            
+      (2, Calendar.January, 2014)
+      (15, Calendar.January, 2014)
+      [Calendar.Monday; Calendar.Friday]
+      [ Vacation((6, Calendar.January, 2014), (10, Calendar.January, 2014));
+        Holiday(13, Calendar.January, 2014)
+      ];;
+    --> [((3, Calendar.January, 2014), Calendar.Friday)]
+*)
 
-let test () =
-  let 
-  (* l1 = (getLectureDates (2, Calendar.January, 2014) (30, Calendar.May, 2014) [Calendar.Monday; Calendar.Friday])
-  and l2 = (getLectureDates (4, Calendar.August, 2014) (13, Calendar.December, 2014) [Calendar.Monday; Calendar.Wednesday])
-  and *)
-  l3 = (getLectureDates (1, Calendar.January, 2015) (3, Calendar.May, 2015) [Calendar.Tuesday; Calendar.Thursday])
+let getLectureDatesWithHolidayList d1 d2 lec hlist =
+  let ldates = (getLectureDates d1 d2 lec)
+  and hlist' = (flattenHolidayList hlist)
   in
+    begin
+      print_string "Holidays - begin\n";
+      printDateList hlist';
+      print_string "Holidays - end\n";
+      (List.filter (fun (d, wd) -> (not (List.mem d hlist'))) ldates)
+    end
+
+(* TEST CASES *)
+let spring2014IIITB = ((2, Calendar.January, 2014), (30, Calendar.May, 2014))
+let autumn2014IIITB = ((4, Calendar.August, 2014) , (13, Calendar.December, 2014))
+let spring2015IIITB = ((1, Calendar.January, 2015), (3, Calendar.May, 2015))
+
+let holidayList2015IIITB = [
+  Holiday(14, Calendar.January, 2015);
+  Holiday(26, Calendar.January, 2015);
+  Holiday(17, Calendar.February, 2015);
+  Holiday(3, Calendar.April, 2015);
+  Holiday(1, Calendar.May, 2015);
+  Holiday(17, Calendar.September, 2015);
+  Holiday(2, Calendar.October, 2015);
+  Holiday(21, Calendar.October, 2015);
+  Holiday(22, Calendar.October, 2015);
+  Holiday(11, Calendar.November, 2015);
+  Holiday(24, Calendar.December, 2015);
+  Holiday(25, Calendar.December, 2015);
+  Vacation((23, Calendar.February, 2015), (9, Calendar.March, 2015));
+  Vacation((4, Calendar.May, 2015), (31, Calendar.May, 2015))
+]
+
+let pl2014 = (getLectureDates (2, Calendar.January, 2014) (30, Calendar.May, 2014) [Calendar.Monday; Calendar.Friday])
+let py2014 = (getLectureDates (4, Calendar.August, 2014) (13, Calendar.December, 2014) [Calendar.Monday; Calendar.Wednesday])
+let st2015 =
+  let (d1, d2) = spring2015IIITB
+  in
+    (getLectureDatesWithHolidayList d1 d2 [Calendar.Tuesday; Calendar.Thursday] holidayList2015IIITB) (* Software Testing Spring 2015 *)
+
+let sriganesh2015 =
+  let (d1, d2) = spring2015IIITB
+  in
+    (getLectureDatesWithHolidayList d1 d2 [Calendar.Tuesday] holidayList2015IIITB) (* PE with Sriganesh and Rishab, Spring 2015 *)
+ 
+let test () =
 (*
   printDateList l1; print_string "\n\n";
   printDateList l2; print_string "\n\n";
+  printDateWeekdayList st2015; print_string "\n\n";
 *)
-  printDateList l3; print_string "\n\n"
+  printDateWeekdayList sriganesh2015; print_string "\n\n"
 
 let _ = test()
